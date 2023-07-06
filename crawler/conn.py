109863ca57a88e612dfeeb7a42d8e7cf146d9f50
slocksert <rsdlj01@gmail.com>
2023-07-05 21:12:20 -0300

    changing env variables and date format

--- crawler/conn.py
+++ crawler/conn.py
@@ -8,29 +8,26 @@ class Con():
     def __init__(self) -> None:
         dotenv.load_dotenv()
         self.host = os.getenv('MYSQL_HOST')
-        self.user = os.getenv('MYSQL_USER')
         self.database = os.getenv('MYSQL_DATABASE')
-        self.password = os.getenv('MYSQL_PASSWORD')
+        self.password = os.getenv('MYSQL_ROOT_PASSWORD')
         self.port = os.getenv('MYSQL_PORT')
 
     def connect(self):
-        self.engine = ce(f'mysql+pymysql://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}')
+        self.engine = ce(f'mysql+pymysql://root:{self.password}@{self.host}:{self.port}/{self.database}')
         self.con = self.engine.connect()
         
     def create(self):
         self.con.execute('CREATE DATABASE IF NOT EXISTS dbroot')
 
     def send_file(self):
-        #current date
         current_date = str(datetime.utcnow().date())
-        n_date = datetime.strptime(current_date, "%Y-%m-%d").strftime("%d-%m-%Y")
-        df = pd.read_csv(f'./csvs/G1_{n_date}.csv')
-        df.to_sql(name='crawler', con=self.engine, if_exists='append', index=False)
-        print(f'The file G1_{n_date}.csv has been sent to the DataBase!')
+        df = pd.read_csv(f'./csvs/G1_{current_date}.csv')
+        df.to_sql(name='crawler', con=self.engine, if_exists='replace', index=False)
+        print(f'The file G1_{current_date}.csv has been sent to the DataBase!')
 
-p = Con()
-p.connect()
-p.create()
-p.send_file()
+database = Con()
+database.connect()
+database.create()
+database.send_file()
 
 
