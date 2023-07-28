ea92647ce82f4413d3ffd1d03f936cd1f8a24a8b
slocksert <rsdlj01@gmail.com>
2023-07-28 12:41:36 -0300

    removing create function and adding args to_sql

--- crawler/conn.py
+++ crawler/conn.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import sqlalchemy
 from sqlalchemy import create_engine as ce
 import pandas as pd 
 import dotenv
@@ -15,19 +16,20 @@ class Con():
     def connect(self):
         self.engine = ce(f'mysql+pymysql://root:{self.password}@{self.host}:{self.port}/{self.database}')
         self.con = self.engine.connect()
-        
-    def create(self):
-        self.con.execute('CREATE DATABASE IF NOT EXISTS dbroot')
 
     def send_file(self):
         current_date = str(datetime.utcnow().date())
         df = pd.read_csv(f'./csvs/G1_{current_date}.csv')
-        df.to_sql(name='crawler', con=self.engine, if_exists='replace', index=False)
+        df.to_sql(name=f'{self.database}', con=self.engine, if_exists='replace', index=True, dtype={
+            'index': sqlalchemy.Integer(),
+            "title": sqlalchemy.String(),
+            "url": sqlalchemy.String(),
+            "date": sqlalchemy.Date()
+        })
         print(f'The file G1_{current_date}.csv has been sent to the DataBase!')
 
 database = Con()
 database.connect()
-database.create()
 database.send_file()
 
 
